3d10d112bd82a74749ee53b3518c24d6029e8ac9
AlexNibley <alexnibley@gmail.com>
2014-04-30 14:06:32 -0600

    color support

--- test.js
+++ test.js
@@ -1,49 +1,59 @@
 var w;
 var h;
 
-var player = [0,0];
+var player = {body: "@", color: "FF0000", pos: [0,0]};
+var ground = {body: "#", color: "0000CC"};
 
 function main() {
   var world = [];
   var colors = [];
-  for(var i=0; i<=h; i++) {
-    var row = [];
-    for(var j=0; j<=w; j++) {
-      row.push("a");
-    }
-    world.push(row);
+
+  function add_entity(entity, pos) {
+    var x = pos[0]; var y = pos[1];
+    world[x][y] = entity.body;
+    colors[x][y] = entity.color;
   }
 
-  world[player[1]][player[0]] = "@";
+  for(var i=0; i<h; i++) {
+    world[i] = [];
+    colors[i] = [];
 
-  //you shouldn't need to change below here, instead insert things while it's still an array, above
-  var world_str = world.map(function(row){
-    return row.map(function(x){
-      return "<span style='color: #"+x+";'>"+x+"</span>";
-    }).join("");
-  }).join("<br />");
+    for(var j=0; j<w; j++) {
+      add_entity(ground, [i, j]);
+    }
+  }
+
+  add_entity(player, player.pos);
+
+  var world_str = "";
+  for(var i=0; i<h; i++) {
+    for(var j=0; j<w; j++) {
+      world_str += "<span style='color: #"+colors[i][j]+";'>"+world[i][j]+"</span>";
+    }
+    world_str += "<br />";
+  }
   document.getElementById("game").innerHTML = world_str;
 }
 
 document.addEventListener("keydown", function(event) {
     if (event.keyCode == 37) { //left
-      if(player[0] > 0) {
-        player[0]--;
+      if(player.pos[1] > 0) {
+        player.pos[1]--;
       }
     }
     else if (event.keyCode == 38) { //up
-      if(player[1] > 0) {
-        player[1]--;
+      if(player.pos[0] > 0) {
+        player.pos[0]--;
       }
     }
     else if (event.keyCode == 39) { //right
-      if(player[0] < w) {
-        player[0]++;
+      if(player.pos[1] < w-1) {
+        player.pos[1]++;
       }
     }
     else if (event.keyCode == 40) { //down
-      if(player[1] < h) {
-        player[1]++;
+      if(player.pos[0] < h-1) {
+        player.pos[0]++;
       }
     }
 }, true);
