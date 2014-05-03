33a12298c8a0afff202283819cee8a3a5e3fdf76
MadcowDeity <carlostnb@gmail.com>
2014-05-02 18:19:47 -0600

    Creating the basic game loop structure

---
+++ game/Game.py
@@ -0,0 +1,19 @@
+class Game:
+    """The primary game class which includes a running loop"""
+    def __init__(self, *args):
+        #TODO: Add as needed
+        pass
+
+    def input(self):
+        """Handle input"""
+        pass
+
+    def update(self, ticksElapsed):
+        """Updates the game"""
+        pass
+
+    def render(self, ticksElapsed):
+        """Renders the game"""
+        pass
+
+
